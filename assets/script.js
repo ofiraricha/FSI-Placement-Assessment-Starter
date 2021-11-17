eaed3d81ccd1dcd6ac6bf01880a6e8f05fe13d2c
Ofir Aricha <92839312+ofiraricha@users.noreply.github.com>
2021-11-16 21:16:02 -0800

    Initial Commit
    8th Module Assessment

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,63 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
+let yourName = "Ofir Aricha"
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
+var total = gb + cc + sugar;
+
+document.getElementById('add-gb').addEventListener('click', function() {
+    gb++
+    document.getElementById('qty-gb').innerHTML = gb; 
+    var total = gb + cc + sugar;
+    document.getElementById('qty-total').innerHTML = total
+})
+
+document.getElementById('minus-gb').addEventListener('click', function(x) {
+    if (gb > 0) {
+        gb--
+        document.getElementById('qty-gb').innerHTML = gb;
+        var total = gb + cc + sugar;
+        document.getElementById('qty-total').innerHTML = total
+    }   
+})
+
+document.getElementById('add-cc').addEventListener('click', function() {
+    cc++
+    document.getElementById('qty-cc').innerHTML = cc;
+    var total = gb + cc + sugar;
+    document.getElementById('qty-total').innerHTML = total
+})
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    if (cc > 0) {
+        cc--
+        document.getElementById('qty-cc').innerHTML = cc;
+        var total = gb + cc + sugar;
+        document.getElementById('qty-total').innerHTML = total
+    }   
+})
+
+document.getElementById('add-sugar').addEventListener('click', function() {
+    sugar++
+    document.getElementById('qty-sugar').innerHTML = sugar;
+    var total = gb + cc + sugar;
+    document.getElementById('qty-total').innerHTML = total
+})
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    if (sugar > 0) {
+        sugar--
+        document.getElementById('qty-sugar').innerHTML = sugar;
+        var total = gb + cc + sugar;
+        document.getElementById('qty-total').innerHTML = total
+    }   
+})
+
+
+
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
